190d54c02c1f8b1dea524f301f5d145a368b04de
theendercore <theendercore@gmail.com>
2023-12-29 16:09:02 +0200

    fixed toml

--- fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/LevelMixin.java
+++ fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/LevelMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
